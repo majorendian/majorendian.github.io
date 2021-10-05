6cf0c6abe78378c48fb0b1e5ee06b32d64019177
Ernest Deak <gordon.zar@gmail.com>
2021-10-05 15:56:07 +0200

    Basic info for page

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+function unscramble(){
+  var text = "jrugrq1}duCjpdlo1frp";
+  var un = "";
+  for (let idx = 0; idx < text.length; idx++) {
+    un += String.fromCharCode(text.charCodeAt(idx) - 3);
+  }
+  return un;
+}
+
+function onLoad(){
+  var elem = document.getElementById("emailp");
+  var text = elem.innerHTML.replace("[%EMAIL%]", unscramble());
+  elem.innerHTML = text;
+}
