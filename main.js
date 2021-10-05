6e660d3e034f9fde315544adb0e6e58e8cc0bade
Ernest Deak <gordon.zar@gmail.com>
2021-10-05 19:56:28 +0200

    updated with custom static site generator

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+const code_links = {
+  "factorial.pl" : "https://gist.githubusercontent.com/majorendian/c101825ba8ef4eeaf067ce4618a1d5d1/raw/56f1b9fbc50f6505cefc33ab19fcf238b6ccb12e/factorial.pl"
+};
+
+function onLoad(){
+  var elem = document.getElementById("emailp");
+  if(elem){
+    var text = elem.innerHTML.replace("[%EMAIL%]", unscramble());
+    elem.innerHTML = text;
+  }
+}
+
+function populate_code(){
+  for(const [key, val] of Object.entries(code_links)){
+    fetchCode(val, key);
+  }
+}
+
+function populate_pages(){
+  const v =["./pages.html", "../pages.html"];
+  v.map(function (page) {
+    console.log(page);
+    fetch(page).then(function (r){return r.text()})
+      .then(function (d){
+        const e = document.getElementById("pages");
+        if(e){
+          e.innerHTML = d;
+        }
+      }).catch(function (e){})
+  })
+}
+
 function unscramble(){
   var text = "jrugrq1}duCjpdlo1frp";
   var un = "";
@@ -7,8 +39,14 @@ function unscramble(){
   return un;
 }
 
-function onLoad(){
-  var elem = document.getElementById("emailp");
-  var text = elem.innerHTML.replace("[%EMAIL%]", unscramble());
-  elem.innerHTML = text;
+function fetchCode(link, codename){
+  fetch(link)
+    .then(function (r){
+      return r.text();
+    }).then(function (r) {
+      const e = document.getElementById(codename);
+      if(e){
+        e.innerHTML = r;
+      }
+    });
 }
